e01d5b762d0ab674e7bf5315e8ecd92a067d1944
吴启凌 <wuqiling97@163.com>
2018-05-06 15:54:41 +0800

    lab6: move lab5 changes

--- labcodes/lab6/kern/trap/trap.c
+++ labcodes/lab6/kern/trap/trap.c
@@ -57,6 +57,16 @@ idt_init(void) {
      /* LAB5 YOUR CODE */ 
      //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
      //so you should setup the syscall interrupt gate in here
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < 256; i ++) {
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+    }
+    // T_SWITCH_TOK号向量是从kernel -> user
+    SETGATE(idt[T_SWITCH_TOK], 1, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
+    SETGATE(idt[T_SYSCALL], 1, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);    
+    // 向CPU写入idt地址，长度
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -229,6 +239,12 @@ trap_dispatch(struct trapframe *tf) {
          * IMPORTANT FUNCTIONS:
 	     * sched_class_proc_tick
          */
+        ticks++;
+        if(ticks % TICK_NUM == 0) {
+            // print_ticks();
+            assert(current != NULL);
+            current->need_resched = 1;
+        }
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
