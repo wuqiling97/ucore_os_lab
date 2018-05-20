4cb19e1d9a56fe6884c0a46a08991696d1299661
吴启凌 <wuqiling97@163.com>
2018-05-21 00:15:02 +0800

    lab7 finish

--- labcodes/lab7/kern/sync/check_sync.c
+++ labcodes/lab7/kern/sync/check_sync.c
@@ -179,29 +179,37 @@ void phi_test_condvar (i) {
 
 
 void phi_take_forks_condvar(int i) {
-     down(&(mtp->mutex));
+    down(&(mtp->mutex));
 //--------into routine in monitor--------------
-     // LAB7 EXERCISE1: YOUR CODE
-     // I am hungry
-     // try to get fork
+    // LAB7 EXERCISE1: YOUR CODE
+    // I am hungry
+    // try to get fork
+    state_condvar[i] = HUNGRY;
+    phi_test_condvar(i);
+    if(state_condvar[i] != EATING) {
+        // cprintf("phi_take_forks_condvar: %d don't get fork and wait\n", i);
+        cond_wait(&mtp->cv[i]);
+    }
 //--------leave routine in monitor--------------
-      if(mtp->next_count>0)
-         up(&(mtp->next));
-      else
-         up(&(mtp->mutex));
+    if(mtp->next_count>0)
+        up(&(mtp->next));
+    else
+        up(&(mtp->mutex));
 }
 
-void phi_put_forks_condvar(int i) {
-     down(&(mtp->mutex));
-
+void phi_put_forks_condvar(int i){
+    down(&(mtp->mutex));
 //--------into routine in monitor--------------
-     // LAB7 EXERCISE1: YOUR CODE
-     // I ate over
-     // test left and right neighbors
+    // LAB7 EXERCISE1: YOUR CODE
+    // I ate over
+    // test left and right neighbors
+    state_condvar[i] = THINKING;
+    phi_test_condvar(LEFT);
+    phi_test_condvar(RIGHT);
 //--------leave routine in monitor--------------
-     if(mtp->next_count>0)
+    if (mtp->next_count > 0)
         up(&(mtp->next));
-     else
+    else
         up(&(mtp->mutex));
 }
 
